5bbf440b76be8917429f9062ffa92b78ed58bbbb
ujjwa893 <ujjwalgupta893@gmail.com>
2023-08-15 03:06:13 +0530

    assignment completed

---
+++ day0_solution.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+        int number1=50; // first number
+        int number2=60; //second number
+        int answer=number1+number2; // storing addition result in answer variable
+        System.out.println("addition result="+answer);
+
+
+    }
+}
