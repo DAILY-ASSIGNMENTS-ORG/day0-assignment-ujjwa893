9ec60ea6fedbd96a64b001ee500de189031d33f4
ujjwa893 <ujjwalgupta893@gmail.com>
2023-08-15 03:22:32 +0530

    assignment completed

--- day0_solution.java
+++ day0_solution.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
 
-        int number1=50; // first number
-        int number2=60; //second number
+
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Input First number");
+        int number1=sc.nextInt(); // first number
+        System.out.println("Input second number");
+        int number2=sc.nextInt(); //second number
         int answer=number1+number2; // storing addition result in answer variable
         System.out.println("addition result="+answer);
+
 
 
     }
